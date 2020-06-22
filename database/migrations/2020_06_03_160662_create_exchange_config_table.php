4dab2ca8ba39ea8adce31a3077ead9dcb140e0f4
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-06-22 14:30:05 +0800

    up...

--- database/migrations/2020_06_03_160662_create_exchange_config_table.php
+++ database/migrations/2020_06_03_160662_create_exchange_config_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateExchangeConfig extends Migration
+class CreateExchangeConfigTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,6 +13,9 @@ class CreateExchangeConfig extends Migration
      */
     public function up()
     {
+        if (Schema::hasTable("exchange_configs")) {
+            return;
+        }
         Schema::create('exchange_configs', function (Blueprint $table) {
             $table->bigIncrements("id");
             $table->integer('num')->index();
