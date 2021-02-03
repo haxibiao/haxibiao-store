ecfa86a75fa2af58102df8ae672463295ba24c70
蔡志刚 <caizhigang@haxibiao.com>
2021-02-03 19:53:09 +0800

    up...

--- database/migrations/2020_04_22_005439_add_product_id_to_image_table.php
+++ database/migrations/2020_04_22_005439_add_product_id_to_image_table.php
@@ -16,10 +16,10 @@ class AddProductIdToImageTable extends Migration
         Schema::table('images', function (Blueprint $table) {
             //方便nova后台上传相关图片，多对多查询全部图片建立关联，数据库会吃不消
             if (!Schema::hasColumn('images', 'store_id')) {
-                $table->Integer('store_id')->index()->comment('店铺id');
+                $table->Integer('store_id')->index()->nullable()->comment('店铺id');
             }
             if (!Schema::hasColumn('images', 'product_id')) {
-                $table->Integer('product_id')->index()->comment('商品id');
+                $table->Integer('product_id')->index()->nullable()->comment('商品id');
             }
         });
     }
