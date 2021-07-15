e5830e2389ed8e6276e8fd0a0a4da68c460abe15
wuxiuzhang <wuxiuzhang@haxibiao.com>
2021-07-15 17:48:19 +0800

    store的item要被task的item吃掉

--- src/Traits/ItemResolvers.php
+++ src/Traits/ItemResolvers.php
@@ -2,24 +2,7 @@
 
 namespace Haxibiao\Store\Traits;
 
-use App\Item;
-
 trait ItemResolvers
 {
 
-    /**
-     * 领取道具
-     */
-    public function resolveReceiveItem($root, array $args, $context, $info)
-    {
-        return Item::receiveItem(getUser(), $args['id']);
-    }
-
-    /**
-     * 我的道具
-     */
-    public function resolveMyItems($root, array $args, $context, $info)
-    {
-        return Item::myItems();
-    }
 }
